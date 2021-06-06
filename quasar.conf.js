cc3719d26d4db49edc412754a51854365f375532
Patrick Monteiro <eng.patrickmonteiro@gmail.com>
2021-06-06 16:53:35 -0300

    add pt-br translate in components

--- quasar.conf.js
+++ quasar.conf.js
@@ -81,7 +81,7 @@ module.exports = function (/* ctx */) {
     // https://v1.quasar.dev/quasar-cli/quasar-conf-js#Property%3A-framework
     framework: {
       iconSet: 'material-icons', // Quasar icon set
-      lang: 'en-us', // Quasar language pack
+      lang: 'pt-br', // Quasar language pack
       config: {},
 
       // Possible values for "importStrategy":
